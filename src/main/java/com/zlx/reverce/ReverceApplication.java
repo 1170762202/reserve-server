82b408b6cc78650f4040d0ffc0e1101b5f2b9a23
zlx <1170762202@qq.ccom>
2019-10-18 12:15:48 +0800

    websocket

--- src/main/java/com/zlx/reverce/ReverceApplication.java
+++ src/main/java/com/zlx/reverce/ReverceApplication.java
@@ -1,24 +1,33 @@
 package com.zlx.reverce;
 
 import com.spring4all.swagger.EnableSwagger2Doc;
-import javafx.application.Application;
+import com.zlx.reverce.handler.BootNettyServer;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 @SpringBootApplication
 @MapperScan("com.zlx.reverce.mapper")
 @EnableSwagger2Doc
-public class ReverceApplication extends SpringBootServletInitializer {
+public class ReverceApplication extends SpringBootServletInitializer{
+
+
+    @Value("${netty.port}")
+    private int port;
+
+    @Value("${netty.url}")
+    private String url;
+
+
 
     public static void main(String[] args) {
         ConfigurableApplicationContext ctx = SpringApplication.run(ReverceApplication.class, args);
@@ -29,7 +38,9 @@ public class ReverceApplication extends SpringBootServletInitializer {
             int port = tomcatServletWebServerFactory.getPort();
             String contextPath = tomcatServletWebServerFactory.getContextPath();
             System.out.println("http://" + host + ":" + port + contextPath + "/");
-        } catch (UnknownHostException e) {
+
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- src/test/java/com/zlx/reverce/ReverceApplicationTests.java
+++ src/test/java/com/zlx/reverce/ReverceApplicationTests.java
@@ -4,6 +4,9 @@ import com.zlx.reverce.entity.TAddressInfo;
 import com.zlx.reverce.entity.TAddressRoomInfo;
 import com.zlx.reverce.service.ITAddressInfoService;
 import com.zlx.reverce.service.ITAddressRoomInfoService;
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.junit.ContiPerfRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,20 +24,24 @@ public class ReverceApplicationTests {
     ITAddressRoomInfoService itAddressRoomInfoService;
 
 
+    @Rule
+   public ContiPerfRule contiPerfRule = new ContiPerfRule();
+
     @Test
+    @PerfTest(invocations = 5000,threads = 1)//10个线程执行10次
     public void contextLoads() {
 
-//        TAddressInfo addrInfo = new TAddressInfo();
-//        addrInfo.setName("北京朗丽兹西山花园酒店");
-//        addrInfo.setDescription("北京朗丽兹西山花园酒店");
-//        addrInfo.setRoomNumber("18");
-//        addrInfo.setAreaSize("65—1300cm");
-//        addrInfo.setCapacity("50—1300");
-//        addrInfo.setInsideParkNumber("0");
-//        addrInfo.setOutsideParkNumber("0");
-//        addrInfo.setImage("http://www.huixiaomeier.com/data/upload/item/1807/27/5b5ae7cfeb010_s.jpg");
-//        addrInfo.setLevel("五星级酒店");
-//        itAddressInfoService.save(addrInfo);
+        TAddressInfo addrInfo = new TAddressInfo();
+        addrInfo.setAddressName("北京朗丽兹西山花园酒店");
+        addrInfo.setAddressDescription("北京朗丽兹西山花园酒店");
+        addrInfo.setAddressRoomNumber("18");
+        addrInfo.setAddressAreaSize("65—1300cm");
+        addrInfo.setAddressCapacity("50—1300");
+        addrInfo.setAddressInsideParkNumber("0");
+        addrInfo.setAddressOutsideParkNumber("0");
+        addrInfo.setAddressImage("http://www.huixiaomeier.com/data/upload/item/1807/27/5b5ae7cfeb010_s.jpg");
+        addrInfo.setAddressLevel("五星级酒店");
+        itAddressInfoService.save(addrInfo);
 //
 //        String id = addrInfo.getId();
 //        System.out.println(id);
